0320f16c45e0101ef9194d6ffc13611ff4fd6e9a
kun-li <45413597+likun-k@users.noreply.github.com>
2020-08-10 16:09:17 +0800

    add reflection test

---
+++ basic/object-oriented/src/reflection/Application.java
@@ -0,0 +1,12 @@
+package reflection;
+
+import java.lang.reflect.Field;
+
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		//TODO ... 
+		
+	}
+	
+}
---
+++ basic/object-oriented/src/reflection/Users.java
@@ -0,0 +1,32 @@
+package reflection;
+
+import java.util.Date;
+
+public class Users {
+
+    private String name;
+
+    private String password;
+
+    private int age;
+
+    private Date birthday;
+    
+    public void eat(String food) {
+    	System.out.println(name + "吃了" + food);
+    }
+    
+    public void sleep() {
+    	System.out.println(name + "正在睡觉！");
+    }
+
+    public String toString() {
+    	return "User : [" 
+    			+ "name: " + this.name
+    			+ ",password: " + this.password 
+    			+ ",age:" + this.age
+    			+ ",birthday:" + this.birthday
+    			+ "]";
+    }
+
+}
