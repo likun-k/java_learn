968ae81c7cc9a0f5d6ee567497b49829bf23e260
wukuso <wukusolove@gmail.com>
2020-08-10 16:03:27 +0800

    1

--- basic/object-oriented/src/ArrayAndList/Users.java
+++ basic/object-oriented/src/ArrayAndList/Users.java
@@ -6,22 +6,38 @@ public class Users {
 	String name="lisi";
 	int sex=0;
 	
+
+
 	public static void main(String[] args) {
 		
-		String arr[]= {"1","24","abd","777","369","555"};
+		Users[] User = new Users[3];
 		
-		String[] sss = new String[arr.length+1];//创建一个长度比原来大的数组
+		Users user2 = new Users();
+		user2.name = "2";
 		
-		for(int i=0;i<arr.length;i++) {//复制数组内容
-			sss[i]=arr[i];
+		Users user3 = new Users();
+		user3.name = "3";
+		
+		Users user4 = new Users();
+		user4.name = "4";
+		
+		User[0] = user2;
+		User[1] = user3;
+		User[2] = user4;
+		
+		
+		
+		String[] sss = new String[User.length+1];//创建一个长度比原来大的数组
+		
+		for(int i=0;i<User.length;i++) {//复制数组内容
+			sss[i] = User[i].name;
 		}
 		
-		sss[arr.length]="lisi";//将新元素添加到新数组内
+		sss[User.length]="lisi";//将新元素添加到新数组内
 
-		arr=sss;
 		//打印输出
-		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
+		for(int i=0;i<User.length;i++) {
+			System.out.println(User[i]);
 		}
 	}
 }
